2d39d53dee0462aac51c5d3d4c1e3e8a9570f444
CLS0S0R0 <00001084221603sp@al.educacao.sp.gov.br>
2024-11-28 18:02:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    "Programação",
+    "O que é JavaScript?",
+    "Java Script é uma linguagem de programação"
+)
+criaCartao(
+    "Programação",
+    "O que é CSS?",
+    "CSS é uma linguagem de estilização"
+)
+criaCartao(
+    "Espaço",
+    "Qual problema os EUA teve com a caneta no espaço?",
+    "Simples, sem gravidade no espaço a caneta não escreve, porque a tinta não consegue sair do tubo"
+)
+criaCartao(
+    "Geografia",
+    "Quantos estados tem o Estados Unidos?",
+    "50 estados e um distrito federal"
+)
